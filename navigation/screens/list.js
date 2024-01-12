04f1535e11448a4b24a31da4dff4d57282ad2cd7
qobulovasror <qobulovasror0@gmail.com>
2024-01-12 17:13:39 +0500

    add: speak verbs

--- navigation/screens/list.js
+++ navigation/screens/list.js
@@ -12,22 +12,32 @@ import { Audio } from 'expo-av';
 
 import data from "../../data/verbs/irregular_verbs_min.json";
 import full_data from "../../data/verbs/irregular_verbs.json";
-
+import audio_paths from '../../data/imgreq';
 
 function List() {
   const playAutio = async (name = "ate") => {
-    name = name.toLowerCase().split("/")[0]
-    // name = '../../data/audios/'+name+'.mp3'
     try {
-        const { sound } = await Audio.Sound.createAsync(require('../../data/audios/'+name+'.mp3'));
-        await sound.playAsync();
-        
+      let name2 = "";
+      if(name.includes("/")){
+        name2 = name.toLowerCase().split("/")[2]
+      }
+      name = name.toLowerCase().split("/")[0]
+      audio_paths.forEach(async elem => {
+        if(elem.name==name){
+          const { sound } = await Audio.Sound.createAsync(elem.url);
+          await sound.playAsync();
+          return;
+        }
+        // if(name2 && elem.name ==name2){
+        //   const { sound } = await Audio.Sound.createAsync(elem.url);
+        //   await sound.playAsync();
+        //   return;
+        // }
+      });      
     } catch (error) {
         console.log(error);
     }
   };
-  const playMusic = async (fileUri) => {
-}
   return (
     <View style={mainStyle.conatainer}>
       <TextInput style={mainStyle.input} placeholder="Enter verb ... " />
